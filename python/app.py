1100fb752932a9490c51a778e4acd9b428b094c7
Mason Terry <mason.terry@q2ebanking.com>
2020-03-02 09:05:29 -0600

    set up basic routes for todos, users, and lists

--- python/app.py
+++ python/app.py
@@ -1,5 +1,47 @@
-#!/usr/local/bin/python3
+from flask import Flask
+from bson import ObjectId
+from bson.json_util import dumps
+from pymongo import MongoClient
+import os
 
-# Make python backend for todo app
+app = Flask(__name__)
+title = 'Todo app with Flask and MongoDB'
+heading = 'Todo app with Flask and MongoDB'
 
-print('To Do')
+# Set up MongoDB connection
+client = MongoClient('mongodb://127.0.0.1:27017')
+db = client.todo
+
+# API welcome
+@app.route('/')
+
+def welcome():
+  return 'Welcome to our Todo API!'
+
+# Fetching users
+@app.route('/users')
+
+def fetch_users():
+  users = db.users.find({})
+  response = dumps(users)
+  return response
+
+# Fetching lists
+@app.route('/lists')
+
+def fetch_lists():
+  lists = db.lists.find({})
+  response = dumps(lists)
+  return response
+
+# Fetching todos
+@app.route('/todos')
+
+def fetch_todos():
+  todos = db.todos.find({})
+  response = dumps(todos)
+  return response
+
+if __name__ == '__main__':
+  print('App running')
+  app.run()
