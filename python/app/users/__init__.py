2bc8113f367ed4e5ef18a2c4f52eac56f5f1e4d9
Mason Terry <mason.terry@q2ebanking.com>
2020-04-09 17:39:56 -0500

    setting up authentication on backend

--- python/app/users/__init__.py
+++ python/app/users/__init__.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 from bson.json_util import dumps
 from app.db import mongo
 from bcrypt import hashpw, checkpw, gensalt
-
+import jwt
 
 bp = Blueprint('users', __name__, url_prefix='/users')
 CORS(bp)
@@ -21,14 +21,22 @@ def login():
     username = request.json['username']
     password = request.json['password']
     user = mongo.db.users.find_one({'username': username})
-    if user == None:
+    if user is None:
         return {'success': False, 'message': 'We could not find anyone with that username'}, 200
 
     pw_check = checkpw(password.encode('utf8'),
                        user['password'].encode('utf8'))
 
     if pw_check is True:
-
-        return {'success': True, 'message': 'User successfully logged in!', 'user': user, 'token': None}, 200
+        token = jwt.encode(user, 'ocuqpvgtta', algorithm='HS256')
+        # token = '12345'
+        return {'success': True, 'message': 'User successfully logged in!', 'user': user, 'token': token}, 200
     else:
         return {'success': False, 'message': 'The password you entered is not correct'}, 200
+
+
+@bp.route('/verify', methods=['GET'])
+def verify():
+    token = request.headers['token']
+    verified = jwt.decode(token, 'ocuqpvgtta', algorithm='HS256')
+    return verified, 200
