8fd95e21978bd3cda7ebdea0279033a93efe60bf
Mason Terry <mason.terry@q2ebanking.com>
2020-04-13 16:05:56 -0500

    working on async await stuff in python

--- python/app/users/__init__.py
+++ python/app/users/__init__.py
@@ -17,18 +17,18 @@ def users():
 
 
 @bp.route('/login', methods=['POST'])
-def login():
+async def login():
     username = request.json['username']
     password = request.json['password']
-    user = mongo.db.users.find_one({'username': username})
+    user = await mongo.db.users.find_one({'username': username})
     if user is None:
         return {'success': False, 'message': 'We could not find anyone with that username'}, 200
 
-    pw_check = checkpw(password.encode('utf8'),
-                       user['password'].encode('utf8'))
+    pw_check = await checkpw(password.encode('utf8'),
+                             user['password'].encode('utf8'))
 
     if pw_check is True:
-        token = jwt.encode(user, 'ocuqpvgtta', algorithm='HS256')
+        token = await jwt.encode(user, 'ocuqpvgtta', algorithm='HS256')
         # token = '12345'
         return {'success': True, 'message': 'User successfully logged in!', 'user': user, 'token': token}, 200
     else:
@@ -36,7 +36,7 @@ def login():
 
 
 @bp.route('/verify', methods=['GET'])
-def verify():
+async def verify():
     token = request.headers['token']
-    verified = jwt.decode(token, 'ocuqpvgtta', algorithm='HS256')
+    verified = await jwt.decode(token, 'ocuqpvgtta', algorithms=['HS256'])
     return verified, 200
